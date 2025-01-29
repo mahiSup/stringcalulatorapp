a1383ae17319350d43efcbb1f09e258547cf94a1
Mahendra <mahi.rtu10@gmail.com>
2025-01-29 15:42:15 +0530

    multiple string numbers and newline delimiter test added.

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,17 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
+    // Check for custom delimiter
+    if (numbers.startsWith("//")) {
+      final delimiterLine = numbers.split('\n')[0];
+      final delimiter = delimiterLine.substring(2);
+      numbers = numbers.substring(delimiterLine.length + 1);
+      numbers = numbers.replaceAll(delimiter, ',');
+    } else {
+      // Replace newlines with commas for normal cases
+      numbers = numbers.replaceAll(RegExp(r'[\n]'), ',');
+    }
+
     final numberStrings = numbers.split(',');
     List<int> negatives = [];
     int sum = 0;
@@ -20,4 +31,6 @@ class StringCalculator {
     return sum;
   }
 }
+
+
 
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -17,6 +17,13 @@ void main() {
     test('should return the number for a single number', () {
       expect(calculator.add('1'), equals(1));
     });
+    test('should return the sum of multiple numbers', () {
+      expect(calculator.add('1,2,3'), equals(6));
+    });
+    test('should handle newlines between numbers', () {
+      expect(calculator.add('1\n2,3'), equals(6));
+    });
+
 
 
   });
